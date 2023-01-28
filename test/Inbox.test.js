5062970da89a5e646be9a29cabc5093544281659
Đăng <103961304+dang-ng@users.noreply.github.com>
2023-01-28 14:17:14 +0000

    fix packages

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,20 +2,23 @@ const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
+const { interface, bytecode } = require('../compile')
 
-class Car {
-    drive(){
-        return 'start'
-    }
-    park(){
-        return 'stop'
-    }
-}
+let account
+let inbox
 
-describe("Car",()=>{
-    it("can drive",()=>{
-        const car = new Car()
-        assert.equal(car.drive(),"start")
+beforeEach(async ()=>{
+    account = await web3.eth.getAccounts()
+    inbox = await new web3.eth.Contract(JSON.parse(interface)).deploy({data:bytecode,arguments:['hi there']}).send({from:account[0],gas:'1000000'})
+})
+describe('Inbox',()=>{
+    it('test1',()=>{
+        assert.ok(inbox.options.address)
+    })
+    it('check default value',async ()=>{
+        assert.equal(await inbox.methods.message().call(),'hi there')
+    })
+    it('set message',async ()=>{
+        assert.ok(await inbox.methods.setMessage("hi derek").send({from:account[0]}))
     })
 })
-
